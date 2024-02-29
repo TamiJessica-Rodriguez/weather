b8a170c7e47dc876b575c98a4f61c7c7a7d5f449
Tami Rodriguez <113520005+TamiJessica-Rodriguez@users.noreply.github.com>
2024-02-29 15:36:55 +0100

    Divided Adventures component into Adventures.ts and Adventures.tsx.

---
+++ src/Adventures.ts
@@ -0,0 +1,40 @@
+// Adventures.ts
+export type Trips = {
+  id: string;
+  title: string;
+  url: string;
+  description: string;
+};
+
+export const mockedAdventures: Trips[] = [
+  // Your mock adventures data
+  {
+    id: "1",
+    title: "Surfing",
+    url: "https://images.unsplash.com/photo-1682687218608-5e2522b04673?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8",
+    description:
+      "Dive into the exhilarating world of surfing on pristine waves, where every ride promises an unforgettable adventure...",
+  },
+  {
+    id: "2",
+    title: "Adventure 2",
+    url: "https://images.unsplash.com/photo-1600466892162-a6b3a60cd9d9?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGZsYW1lbmNvfGVufDB8fDB8fHww",
+    description:
+      "Celebrate the vibrant spirit of Flamenco at our city's Feria! Immerse yourself in the rhythm of traditional music...",
+  },
+  {
+    id: "3",
+    title: "Adventure 3",
+    url: "https://media.istockphoto.com/id/1361421117/sv/foto/hiking-couple-climb-up-mountain-ridge.jpg?s=612x612&w=0&k=20&c=Tds4-DsgrCg3E61697QtzmYaqObjMrvcnesy2tLbEcI=",
+    description:
+      "Ascend to new heights right in the heart of the city! Experience the thrill of mountain climbing against a breathtaking urban backdrop...",
+  },
+  {
+    id: "4",
+    title: "Adventure 3",
+    url: "https://media.istockphoto.com/id/1361421117/sv/foto/hiking-couple-climb-up-mountain-ridge.jpg?s=612x612&w=0&k=20&c=Tds4-DsgrCg3E61697QtzmYaqObjMrvcnesy2tLbEcI=",
+    description:
+      "Ascend to new heights right in the heart of the city! Experience the thrill of mountain climbing against a breathtaking urban backdrop...",
+  },
+  // Additional adventures omitted for brevity
+];
